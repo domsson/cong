462e160b0ff75ed29da5d5f60d5a34d21541d53c
Julien <julien.dau@student.htw-berlin.de>
2015-06-30 00:46:41 +0200

    Moved all source files into the src directory and amended the build script accordingly

--- build.sh
+++ build.sh
@@ -1,10 +1,5 @@
-# g++ -c main.cpp -o main.o
-# g++ -c Game.cpp -o Game.o
-# g++ -c Ball.cpp -o Ball.o
-# g++ -c Court.cpp -o Court.o
-# g++ -c Paddle.cpp -o Paddle.o
-# g++ -c ScoreDisplay.cpp -o ScoreDisplay.o
-
+cd src
 g++ -c *.cpp -std=c++11
-g++ -o cong *.o -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
+g++ -o ../cong *.o -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
 rm ./*.o
+cd ..
